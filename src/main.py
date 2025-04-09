0ae2a104d400446a9a4bb9a58069827669db87bd
kassvl <melture33@gmail.com>
2025-04-09 12:48:56 +0200

    Kod kalitesi hatalaru0131 du00fczeltildi

--- src/main.py
+++ src/main.py
@@ -4,5 +4,9 @@ def main():
     print("Hello, Git Workflows and GitHub Actions!")
     print(new_feature())
 
+def hello_world():
+    print("Hello, world!") # Girinti du00fczeltildi
+
 if __name__ == "__main__":
     main()
+    hello_world()
